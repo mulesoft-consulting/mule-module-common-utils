f65a097af34c79e24401d2f3cac4aa8db4ca549d
javaduke <eugene.berman@mulesoft.com>
2012-07-25 09:31:58 -0700

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "com.mulesoft.module.utils", "com/mulesoft/module/utils/package-summary.html", [ [ "Classes", null, [ [ "ThrottleQueueManager", "com/mulesoft/module/utils/ThrottleQueueManager.html", null, "" ], [ "UtilsModule", "com/mulesoft/module/utils/UtilsModule.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.mulesoft.module.utils.adapters", "com/mulesoft/module/utils/adapters/package-summary.html", [ [ "Classes", null, [ [ "UtilsModuleCapabilitiesAdapter", "com/mulesoft/module/utils/adapters/UtilsModuleCapabilitiesAdapter.html", null, "" ], [ "UtilsModuleInjectionAdapter", "com/mulesoft/module/utils/adapters/UtilsModuleInjectionAdapter.html", null, "" ], [ "UtilsModuleLifecycleAdapter", "com/mulesoft/module/utils/adapters/UtilsModuleLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.mulesoft.module.utils.config", "com/mulesoft/module/utils/config/package-summary.html", [ [ "Classes", null, [ [ "ThrottleDefinitionParser", "com/mulesoft/module/utils/config/ThrottleDefinitionParser.html", null, "" ], [ "UtilsModuleConfigDefinitionParser", "com/mulesoft/module/utils/config/UtilsModuleConfigDefinitionParser.html", null, "" ], [ "UtilsModuleNamespaceHandler", "com/mulesoft/module/utils/config/UtilsModuleNamespaceHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "com.mulesoft.module.utils.processors", "com/mulesoft/module/utils/processors/package-summary.html", [ [ "Classes", null, [ [ "ThrottleMessageProcessor", "com/mulesoft/module/utils/processors/ThrottleMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ java/lists.js
@@ -0,0 +1,17 @@
+var DATA = [
+      { id:0, label:"com.mulesoft.module.utils", link:"java/com/mulesoft/module/utils/package-summary.html", type:"package" },
+      { id:1, label:"com.mulesoft.module.utils.ThrottleQueueManager", link:"java/com/mulesoft/module/utils/ThrottleQueueManager.html", type:"class" },
+      { id:2, label:"com.mulesoft.module.utils.UtilsModule", link:"java/com/mulesoft/module/utils/UtilsModule.html", type:"class" },
+      { id:3, label:"com.mulesoft.module.utils.adapters", link:"java/com/mulesoft/module/utils/adapters/package-summary.html", type:"package" },
+      { id:4, label:"com.mulesoft.module.utils.adapters.UtilsModuleCapabilitiesAdapter", link:"java/com/mulesoft/module/utils/adapters/UtilsModuleCapabilitiesAdapter.html", type:"class" },
+      { id:5, label:"com.mulesoft.module.utils.adapters.UtilsModuleInjectionAdapter", link:"java/com/mulesoft/module/utils/adapters/UtilsModuleInjectionAdapter.html", type:"class" },
+      { id:6, label:"com.mulesoft.module.utils.adapters.UtilsModuleLifecycleAdapter", link:"java/com/mulesoft/module/utils/adapters/UtilsModuleLifecycleAdapter.html", type:"class" },
+      { id:7, label:"com.mulesoft.module.utils.config", link:"java/com/mulesoft/module/utils/config/package-summary.html", type:"package" },
+      { id:8, label:"com.mulesoft.module.utils.config.ThrottleDefinitionParser", link:"java/com/mulesoft/module/utils/config/ThrottleDefinitionParser.html", type:"class" },
+      { id:9, label:"com.mulesoft.module.utils.config.UtilsModuleConfigDefinitionParser", link:"java/com/mulesoft/module/utils/config/UtilsModuleConfigDefinitionParser.html", type:"class" },
+      { id:10, label:"com.mulesoft.module.utils.config.UtilsModuleNamespaceHandler", link:"java/com/mulesoft/module/utils/config/UtilsModuleNamespaceHandler.html", type:"class" },
+      { id:11, label:"com.mulesoft.module.utils.processors", link:"java/com/mulesoft/module/utils/processors/package-summary.html", type:"package" },
+      { id:12, label:"com.mulesoft.module.utils.processors.ThrottleMessageProcessor", link:"java/com/mulesoft/module/utils/processors/ThrottleMessageProcessor.html", type:"class" },
+      { id:13, label:"throttle", link:"mule/utils.html#throttle", type:"method" }
+
+    ];
